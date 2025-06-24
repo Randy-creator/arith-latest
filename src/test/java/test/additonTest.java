714c45384c5c35f835a80bbfa127a3dc978c0e03
Randy-creator <hei.randy.2@gmail.com>
2025-06-24 08:49:14 +0300

    fix: no overflow on addition

--- src/test/java/test/additonTest.java
+++ src/test/java/test/additonTest.java
@@ -1,9 +1,9 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-
 import hei.school.demo.endpoint.rest.controller.health.AdditionController;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class additonTest {
   private final AdditionController subject = new AdditionController();
@@ -11,5 +11,10 @@ public class additonTest {
   @Test
   void additionTest() {
     assertEquals(3, subject.addition(1, 2));
+  }
+
+  @Test
+  void addition_no_overflow() {
+    assertEquals(3_000_000_000l, subject.addition(1_000_000_000, 2_000_000__000));
   }
 }
